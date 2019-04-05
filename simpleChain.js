7c873e756341a7470e781ebab84440e22a1ca19a
s2010 <bashayer.hijji@gmail.com>
2019-04-05 16:17:33 +0300

    Storing & Linking Blocks

--- simpleChain.js
+++ simpleChain.js
@@ -1,3 +1,9 @@
+/* ===== SHA256 with Crypto-js ===================================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js      |
+|  =============================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
 // ===== STEP 1: Block data model =================
 // ===== Block Class ==============================
 // ===== init block obj ===========================
@@ -25,10 +31,19 @@ class Block{
 class Blockchain{
     constructor(){
       this.chain = [];
-      this.addBlock(new Block("First block in the chain - Genesis block"));
+      this.addBlock(this.createGenesisBlock());
     }
+    createGenesisBlock(){
+        return new Block("First block in the chain - Genesis block");
+    }    
     // Add new block
     addBlock(newBlock){
+      // previous block hash
+      if(this.chain.length>0){
+        newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+      }  
+      // Block hash with SHA256 using newBlock and converting to a string
+      newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();  
       // Adding block object to chain
       this.chain.push(newBlock);
     }
